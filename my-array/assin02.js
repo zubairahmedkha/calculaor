8c95f3762a9cdb884d27b9f3641e8c723522a2c2
zubairkhan <zubairahmedniz@gmail.com>
2024-03-08 14:23:18 +0500

    assinmet02

---
+++ my-array/assin02.js
@@ -0,0 +1,5 @@
+"use strict";
+let nam = "zubair Ahmed";
+console.log(nam.toLocaleLowerCase());
+console.log(nam.toUpperCase());
+console.log(nam.charAt(0).toLocaleUpperCase() + nam.slice(1));
---
+++ my-array/assin02.ts
@@ -0,0 +1,7 @@
+let nam="zubair Ahmed";
+console.log(nam.toLocaleLowerCase());
+console.log(nam.toUpperCase());
+console.log(nam.charAt(0).toLocaleUpperCase()+nam.slice(1));
+
+
+
