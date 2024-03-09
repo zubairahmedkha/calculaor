f47968668c3a9dd92687f638b24e98344722ac3f
zubairkhan <zubairahmedniz@gmail.com>
2024-03-09 10:40:49 +0500

    arithmetic work

---
+++ my-array/array.js
@@ -0,0 +1,22 @@
+var fruit = ["mango", "apple", "bannana", "orange"];
+fruit[4] = "cat";
+fruit.pop();
+console.log(fruit);
+var n1 = 20;
+var n2 = 2;
+console.log(n1 / n2);
+var n3 = 20;
+var n4 = 2;
+console.log(n3 % n4);
+var n = 15;
+n += 5;
+n -= 5;
+console.log(n); // 10
+//let n5 = 5;
+//console.log(n5 == 5); /true
+var n5 = 5;
+console.log(n5 === 5);
+console.log(n5 != 5);
+console.log(n5 <= 8);
+console.log(!(n5 > 10));
+//console.log(!(n5 < 10)); 
---
+++ my-array/array.ts
@@ -0,0 +1,24 @@
+let fruit = ["mango","apple","bannana","orange"]
+fruit[4]="cat"
+fruit.pop()
+console.log(fruit)
+let n1 = 20;
+ let n2 = 2;
+console.log(n1 /n2); 
+let n3 = 20;
+let n4 = 2;
+console.log(n3 % n4); 
+let n = 15;
+n += 5;
+n -= 5;
+console.log(n); // 10
+//let n5 = 5;
+//console.log(n5 == 5); /true
+let n5 = 5;
+console.log(n5 === 5);
+console.log(n5 != 5); 
+console.log(n5 <= 8); 
+console.log(!(n5 > 10))
+//console.log(!(n5 < 10)); 
+
+
