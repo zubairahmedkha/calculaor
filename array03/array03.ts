ec6bc684ddbfe6ecc5f82b5f1d8ff4821c5a1324
zubairkhan <zubairahmedniz@gmail.com>
2024-03-17 07:01:33 +0500

    mywork

---
+++ array03/array03.ts
@@ -0,0 +1,29 @@
+//number calculaor
+//function sum (num1:number,num2:number)
+//{//var num1 = Number
+   // var num2 = Number
+    //var result = num1+num2
+   // console.log("result is",result)
+//}
+
+//sum(40,40)
+//sum(34,34)
+ /* let car = {"name ":"swift",
+ "model":"2020",
+ 
+ "color:green":null}
+ 
+ 
+ console.log(car["name "])
+ console.log(car.model)*\\*/
+ let userName ={name:"Ali khan",
+ Email:"essa.com" ,age:90,
+loggIn:true}
+
+ console.log(userName.Email.toUpperCase())
+ console.log(userName.age)
+ console.log(userName.loggIn)
+ console.log(userName.name.toLowerCase())
+
+
+
